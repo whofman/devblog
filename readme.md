4b512d4d79097dc94fad6b9dda1c39082d3546e3
Hans Hofman <hans@hahoweb.nl>
2014-11-02 14:45:37 +0100

    Git Setup

---
+++ readme.md
@@ -0,0 +1,36 @@
+# Baseblog Kirby Theme
+
+This theme is made to give you [a nice and clean start](http://sashtown.de/blog/what-baseblog-is) when it comes to creating a blog with Kirby. It allows you to set up your posts and pages quickly, so you can easily customize it and just start blogging right away. Here are some built-in features of Baseblog:
+
+- post types (texts, links and videos)
+- tags
+- search
+- error page
+- RSS feed
+- pagination for articles
+- static tweet button
+- responsiveness
+- prearranged blueprints for Panel usage
+
+## Installation
+The easiest way to install Kirby with Baseblog is via terminal:
+
+```
+git clone --recursive https://github.com/sashtown/Baseblog-Kirby-Theme.git
+```
+
+If you've already installed Kirby with the Starterkit remove the default `/assets`, `/content` and `/site` folders and replace them with the downloaded ones from Baseblog.
+
+## Kirby?
+[Kirby](http://getkirby.com) is a lightweight, flexible and file-based CMS made by [Bastian Allgeier](http://bastianallgeier.com). If you want to learn more about Kirby, visit the Kirby Website and check out the [docs](http://getkirby.com/docs)!
+
+## License
+This theme is **free to use** with your Kirby installation and licensed under the [MIT License](https://github.com/sashtown/Baseblog-Kirby-Theme/blob/master/license.md). If you still want to support me and the further development of this theme, you can do this [right here](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=9RBGZCYTNR2EC).
+
+Please remember, that Kirby itself is not for free. If you use this theme for a public release, don't forget to [buy a license](http://getkirby.com/buy) for Kirby!
+
+## Showcase
+<http://baseblog.sashtown.de/demo>
+
+## Contact
+If you have any questions about the theme, contact me by [email](mailto:mail@sashtown.de) or on [Twitter](http://twitter.com/sashtown).
---
+++ site/config/config.php
@@ -0,0 +1,88 @@
+<?php
+
+/*
+
+---------------------------------------
+License Setup
+---------------------------------------
+
+Please add your license key, which you've received
+via email after purchasing Kirby.
+
+If you have no license yet, please buy one:
+http://getkirby.com/buy and support an indie developer.
+
+You are not allowed to run a website without a valid license key.
+Please read the End User License Agreement for more information:
+http://getkirby.com/license
+
+*/
+
+c::set('license', 'K2-PERSONAL-a3b2be01a50cd5be5e774ba7fb8b25ed');
+
+/*
+
+---------------------------------------
+Subfolder Setup
+---------------------------------------
+
+Kirby will automatically try to detect the subfolder
+
+i.e. http://yourdomain.com/subfolder
+
+This might fail depending on your server setup.
+In such a case, please set the correct subfolder here.
+
+You must also set the right url then:
+
+c::set('url', 'http://yoururl.com/subfolder');
+
+if you are using the .htaccess file, make sure to
+set the right RewriteBase there as well:
+
+RewriteBase /subfolder
+
+*/
+// RewriteBase /baseblog
+
+c::set('url', 'http://localhost/devblog');
+c::set('rewrite', true);
+
+c::set('subfolder', false);
+
+
+/*
+
+---------------------------------------
+Homepage Setup
+---------------------------------------
+
+By default the folder/uri for your homepage is "home".
+Sometimes it makes sense to change that to make your blog
+your homepage for example. Just change it here in that case.
+
+*/
+
+c::set('home', 'blog');
+
+
+/*
+
+---------------------------------------
+Markdown Setup
+---------------------------------------
+
+You can globally switch Markdown parsing
+on or off here.
+
+To disable automatic line breaks in markdown
+set markdown.breaks to false.
+
+You can also switch between regular markdown
+or markdown extra: http://michelf.com/projects/php-markdown/extra/
+
+*/
+
+c::set('markdown', true);
+c::set('markdown.breaks', true);
+c::set('markdown.extra', true);
